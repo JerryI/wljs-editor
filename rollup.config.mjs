f7d3166919b77bbd9308689123bc98722e2348e7
Kirill <krikus.ms@gmail.com>
2024-03-04 17:51:12 +0100

    minjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,15 +2,23 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
 
+import terser from '@rollup/plugin-terser';
+
 export default {
 
   input: 'src/kernel.js',
   
-  output: {
+  output: [{
+    file: 'dist/kernel.min.js',
+    format: "es",
+    strict: false,
+    manualChunks: () => 'merged',
+    plugins: [terser()]
+  },{
     dir: 'dist/',
     format: "es",
     strict: false
-  },
+  }],
   plugins    : [
   nodeResolve({
     jsnext: true,
